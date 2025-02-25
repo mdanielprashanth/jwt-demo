37218b676626ba5d3356d5d1bec081b1900e267c
Daniel <mdanielprashanth@gmail.com>
2025-02-25 23:36:39 +0530

    intial commit

---
+++ JWT/authserver.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = new express()
+const jwt = require('jsonwebtoken');
+
+
+app.use(express.json())
+
+const port = 9000;
+
+let refreshTokens = [];
+
+app.post('/token', (req, res)=>{  
+
+    const refreshToken = req.body.token;
+    if (refreshToken === null) return res.sendStatus(401);
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (err, user)=>{
+        if(err) return res.sendStatus(403)
+        const acessToken = generateToken({name : user.name})    
+        res.json({acessToken:acessToken})
+    })
+
+
+})
+
+app.post('/login', (req,res)=>{
+
+    //Authenticate user 
+
+    const username = req.body.username;
+    const user = {name: username}
+
+    const acessToken = generateToken(user)
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN)
+    refreshTokens.push(refreshToken)
+    res.json({acessToken:acessToken, refreshToken:refreshToken})
+})
+
+function authorize(req, res, next) {
+    const authorize = req.headers['authorization']
+    const token = authorize && authorize.split(' ')[1]
+    
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, user)=>{
+       if(err) return res.sendStatus(403)
+       console.log(user);
+    
+       req.user = user;
+       next()
+    }) 
+}
+
+
+app.delete('/logout', (req, res)=>{
+    refreshTokens = refreshTokens.filter((token) => token !== req.body.token)
+    res.sendStatus(204)
+})
+
+function generateToken(user){
+    return jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn:'30s'})
+}
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`);
+})
---
+++ JWT/index.js
@@ -0,0 +1,61 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = new express()
+const jwt = require('jsonwebtoken');
+
+
+app.use(express.json())
+
+const port = 8000;
+
+
+const posts = [
+{
+        username: 'daniel',
+        title: 'post 1'
+    },
+    {
+        username: 'Kyle',
+        title: 'post 2'
+    }
+]
+
+app.get('/posts', authorize,  (req, res)=>{
+    
+    res.json(posts.filter(post => post.username === req.user.name));
+
+})
+
+app.post('/login', (req,res)=>{
+
+    //Authenticate user 
+
+    const username = req.body.username;
+    const user = {name: username}
+
+    const acessToken = jwt.sign(user, process.env.ACCESS_TOKEN)
+    res.json({acessToken:acessToken})
+})
+
+
+function authorize(req, res, next) {
+    const authorize = req.headers['authorization']
+    const token = authorize && authorize.split(' ')[1]
+    
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, user)=>{
+       if(err) return res.sendStatus(403)
+       console.log(user);
+    
+       req.user = user;
+       next()
+    }) 
+}
+
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`);
+    
+})
